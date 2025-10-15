7696bdb367071765a157e125aea14a627eb44fa7
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-10-15 20:32:11 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const toggle = document.getElementById("switch");
+
+toggle.addEventListener("change", function() {
+  if (this.checked) {
+    
+    document.body.style.backgroundColor = "";
+    document.body.style.backgroundImage = "url('../assets/nc.jpg";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center";
+  } else {
+     document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "rgb(196, 255, 199)";
+
+    
+  }
+});
